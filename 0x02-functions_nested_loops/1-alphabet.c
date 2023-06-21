695c7f850df2cc4792684cffc7d1a063707ccb3d
Rednax3la <wambugualexander09@gmail.com>
2023-06-21 10:03:48 -0700

    Nesting

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* print_alphabet - print all alphabet in lowercase
+*/
+void print_alphabet(void)
+{
+char l;
+for (l = 'a'; l <= 'z'; l++)
+_putchar(letter);
+_putchar('\n');
+}
